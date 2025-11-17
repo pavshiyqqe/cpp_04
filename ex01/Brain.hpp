affbce2b2ee2abbd795ebeba38f288f3e01277c9
Yevhen Yaniv <yyaniv@f4r12s11.clusters.42paris.fr>
2025-11-17 13:09:46 +0100

    Brain.hpp added

--- ex01/Brain.hpp
+++ ex01/Brain.hpp
@@ -6,7 +6,28 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 13:08:07 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/16 13:08:07 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/17 13:08:41 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+
+class Brain
+{
+    public:
+        Brain();
+        ~Brain();
+        Brain(const Brain &other);
+
+        Brain               &operator=(const Brain &other);
+
+        const std::string   &getIdea(unsigned int index) const;
+        void                addIdea(std::string idea);
+
+    private:
+        std::string     _ideas[100];
+        unsigned int    _size;
+};
