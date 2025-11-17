7c12a95b28c855b9c0678c8c8c93aa1ce4a473b6
Yevhen Yaniv <yyaniv@f4r12s11.clusters.42paris.fr>
2025-11-17 13:20:50 +0100

    Brain.cpp added [ex01]

--- ex01/Brain.cpp
+++ ex01/Brain.cpp
@@ -6,7 +6,46 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/16 13:08:08 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/17 13:08:50 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/17 13:20:28 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Brain.hpp"
+
+Brain::Brain()
+{
+    std::cout << "Default constructor [BRAIN]\n";
+    this->_size = 0;
+}
+
+Brain::~Brain()
+{
+    std::cout << "Destructor [BRAIN]\n";
+}
+
+Brain::Brain(const Brain &other)
+{
+    std::cout << "Copy-constructror [BRAIN]\n";
+    *this = other;
+}
+
+Brain   &Brain::operator=(const Brain &other)
+{
+    int i = 0;
+    while (i < 100)
+        this->_ideas[i] = other._ideas[i++];
+    this->_size = other._size;
+    return (*this);
+}
+
+void                Brain::addIdea(std::string idea)
+{
+    if (this->_size == 100)
+        return;
+    this->_ideas[_size++] = idea;
+}
+
+const std::string   &Brain::getIdea(unsigned int index) const
+{
+    return (this->_ideas[index]);
+}
