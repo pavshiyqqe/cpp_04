3fd6eb945c64291d17a7f027e7691fcc8f422e94
Eugene_pavshiy <keki4noob@gmail.com>
2025-11-17 14:03:43 +0100

    small changes in Dog:cpp|hpp

--- ex01/Dog.hpp
+++ ex01/Dog.hpp
@@ -6,21 +6,26 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 16:21:02 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/15 16:22:06 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/17 13:59:17 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Dog : public Animal
 {
     public:
         Dog();
         ~Dog();
-        Dog (const Dog &);
+        Dog (const Dog &other);
 
-        using   Animal::operator=;
+        Animal  &operator=(const Animal &other);
         void    makeSound() const;
+        Brain   *getBrain() const;
+
+    private:
+        Brain   *_brain;
 };
