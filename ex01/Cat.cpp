48f6e4ae436461913e4274c01da51107576da9d0
Eugene_pavshiy <keki4noob@gmail.com>
2025-11-17 14:09:41 +0100

    [ex01]Cat remake cpp|hpp

--- ex01/Cat.cpp
+++ ex01/Cat.cpp
@@ -6,7 +6,7 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 16:22:14 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/15 16:26:36 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/17 14:09:10 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,41 @@
 Cat::Cat() : Animal("Cat")
 {
     std::cout << "Default constructor of " << this->type <<  "\n";
+    this->_brain = new Brain;
 }
 
-Cat::Cat(const Cat &) : Animal("Cat")
+Cat::Cat(const Cat &other) : Animal("Cat")
 {
     std::cout << "Copy-constructor of " << this->type << "\n";
+    *this = other;
 }
 
 Cat::~Cat()
 {
     std::cout << this->type << " was deleted\n";
+    delete this->_brain;
 }
+
+Animal  &Cat::operator=(const Animal &other)
+{
+    const Cat   *P_other;
+
+    P_other = dynamic_cast<const Cat *>(&other);
+    if (P_other)
+    {
+        this->type = P_other->type;
+        *this->_brain = *P_other->_brain;
+    }
+    return (*this);
+}
+
 
 void    Cat::makeSound() const
 {
     std::cout << "[ Meow! ]\n";
+}
+
+Brain   *Cat::getBrain() const
+{
+    return (this->_brain);
 }
--- ex01/Cat.hpp
+++ ex01/Cat.hpp
@@ -6,21 +6,26 @@
 /*   By: yyaniv <yyaniv@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 16:18:45 by yyaniv            #+#    #+#             */
-/*   Updated: 2025/11/15 16:20:36 by yyaniv           ###   ########.fr       */
+/*   Updated: 2025/11/17 14:06:38 by yyaniv           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "Animal.hpp"
+#include "Brain.hpp"
 
 class Cat : public Animal
 {
     public:
         Cat();
         ~Cat();
-        Cat(const Cat &);
+        Cat(const Cat &other);
         
-        using   Animal::operator=;
+        Animal  &operator=(const Animal &other);
         void    makeSound() const;
+        Brain   *getBrain() const;
+    
+    private:
+        Brain   *_brain;
 };
